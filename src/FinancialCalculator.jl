16e281eb08066f16087919d3e924b7d8e8c0d1fb
熊天锴 <Xiong@xiongtiankais-MacBook-Pro.local>
2019-07-07 11:33:43 +0800

    initial commit

---
+++ src/FinancialCalculator.jl
@@ -0,0 +1,12 @@
+module FinancialCalculator
+
+function get_PV(FV::Real, r::Real, N_Years::Real; compounding::Symbol = :Yearly)
+    if compounding == :Yearly
+        PV = FV / (1+r)^N_Years
+    end
+    return PV
+
+end
+
+
+end # module
